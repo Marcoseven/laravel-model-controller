1aa8d5330a9aa9f13b872308d7b11a214bc3fe69
Marcoseven <marco.caliciotti3@gmail.com>
2022-01-11 17:28:51 +0100

    Added latest changes
    Inserted forelse loop to cycle inside the movies table.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,19 @@
 
 <div class="container">
     <h1>My movies</h1>
+
+    @forelse($movies as $movie)
+    <h3>Titolo film: {{ $movie->title }}</h3>
+    <div class="d-flex">
+        <h4>Titolo originale film: {{ $movie->original_title }}</h4>
+        <h4>Nazionalità film: {{ $movie->nationality }}</h4>
+        <h4>Data uscita film: {{ $movie->date }}</h4>
+        <hr>
+    </div>
+
+    @empty
+    <p>Sorry nothing to movie found </p>
+    @endforelse
 </div>
 
 @endsection
