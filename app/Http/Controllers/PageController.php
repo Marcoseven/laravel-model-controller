1c2c99b7c7bebf9616694691bfd6b95f95f3e253
Marcoseven <marco.caliciotti3@gmail.com>
2022-01-11 17:13:25 +0100

    Added changes
    Created the Movie.php template and modified the PageController.php and web.php pages.

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+    // return home page 
+    public function index()
+    {
+        ddd(Movie::all());
+        return view('home');
+    }
 }
