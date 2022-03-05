ffeeff942af3201a28025bb6ee9a9d7902067e72
Marcoseven <marco.caliciotti3@gmail.com>
2022-03-05 15:08:08 +0100

    Add changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,21 +1,19 @@
 @extends('layouts.app')
 @section('content')
+    <div class="container">
+        <h1>My movies</h1>
 
-<div class="container">
-    <h1>My movies</h1>
+        @forelse($movies as $movie)
+            <h3>Titolo film: {{ $movie->title }}</h3>
+            <div class="d-flex">
+                <h4>Titolo originale film: {{ $movie->original_title }}</h4>
+                <h4>Nazionalità film: {{ $movie->nationality }}</h4>
+                <h4>Data uscita film: {{ $movie->date }}</h4>
+                <hr>
+            </div>
 
-    @forelse($movies as $movie)
-    <h3>Titolo film: {{ $movie->title }}</h3>
-    <div class="d-flex">
-        <h4>Titolo originale film: {{ $movie->original_title }}</h4>
-        <h4>Nazionalità film: {{ $movie->nationality }}</h4>
-        <h4>Data uscita film: {{ $movie->date }}</h4>
-        <hr>
+        @empty
+            <p>Sorry nothing to movie found</p>
+        @endforelse
     </div>
-
-    @empty
-    <p>Sorry nothing to movie found </p>
-    @endforelse
-</div>
-
 @endsection
